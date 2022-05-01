84b7f84e85d72c59c7b5275296e5c5dea7eb85a9
Mayank Mani Pant <102402423+mayankmanipant@users.noreply.github.com>
2022-05-01 11:41:49 +0530

    Create BinaryTree.c

---
+++ Tree/BinaryTree.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node
+{
+  int data;
+  struct node *left,*right;
+};
+
+
+struct node *create()
+{
+  int x;
+  struct node *newnode;
+  newnode=(struct node*)malloc(sizeof(struct node));
+  printf("\nEnter data(-1 for no node):");
+  scanf("%d",&x);
+  if(x==-1)
+  {
+    return 0;
+  }
+  newnode->data=x;
+  printf("\nEnter left child of %d",x);
+  newnode->left=create();
+  printf("\nEnter right child of %d",x);
+  newnode->right=create();
+  return newnode;
+} 
+
+
+void preorder(struct node *root)
+{
+  if(root==NULL)
+  {
+    return;
+  }
+  printf("%d ",root->data);
+  preorder(root->left);
+  preorder(root->right);
+}
+
+
+void inorder(struct node *root)
+{
+  if(root==NULL)
+  {
+    return;
+  }
+  inorder(root->left);
+  printf("%d ",root->data);
+  inorder(root->right);
+}
+
+
+void postorder(struct node *root)
+{
+  if(root==NULL)
+  {
+    return;
+  }
+  postorder(root->left);
+  postorder(root->right);
+  printf("%d ",root->data);
+}
+
+
+int main()
+{
+  struct node *root;
+  root=0;
+  root=create();
+  printf("\nPREORDER:");
+  preorder(root);
+  printf("\nINORDER:");
+  inorder(root);
+  printf("\nPOSTORDER:");
+  postorder(root);
+}
